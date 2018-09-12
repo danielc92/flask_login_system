89c072f71a0645874305df7158702002d05d3b97
daniel corcoran <danielc.92g@gmail.com>
2018-09-12 14:19:06 +1000

    Added login logic

--- app.py
+++ app.py
@@ -7,13 +7,18 @@ from flask import Flask,render_template,url_for,redirect,request,session
 from datetime import timedelta
 from functools import wraps
 import json
+import pandas
+from sqlalchemy import create_engine
 import os
+import time
 
 # App config
 app = Flask(__name__)
 app.secret_key = 'jhgjjGDpifaj4904ufjajlwah89h392hi4lfknhflkh9842hfiu239h72h928b2ih2897b290f420j2khg2984hoklndjn2f3908h209d2'
 app.debug = True
 
+# Create database engine
+engine = create_engine('postgresql://danielcorcoran@localhost:5432/flask')
 
 # Login Required function
 def login_required(f):
@@ -28,23 +33,37 @@ def login_required(f):
 # Login Route
 @app.route('/login', methods=['GET', 'POST'])
 def login():
+    # Reset Error
     error = None
+
     if request.method == 'POST':
-        login = request.form['username']
-        password = request.form['password']
         
-        if test_username == login and test_password == password:
+        posted_login = request.form['username']
+        posted_password = request.form['password']
+        select_skeleton = "SELECT * FROM users WHERE username_ = '{}'"
+        users = pandas.read_sql_query(select_skeleton.format(posted_login), con = engine)
 
-            print(test_username + ' has logged in successfully.')
-            session['logged_in'] = True
-            session['user'] = login
-            return redirect(url_for('home'))
+        print(users)
+        if len(posted_login) == 0 or len(posted_password) == 0:
+            error = "You must enter a username and password to login."
+            return render_template('login.html', error = error)
         else:
-            error = 'Invalid credentials. Try again'
-            return render_template('login.html', error=error)
 
-    return render_template('login.html', error=error)
+            if users.shape[0] > 0:
+                try:
+                    database_password = users.loc[0, 'passowrd_']
+                    print(database_password)
+                    session['logged_in'] = True
+                    session['username'] = posted_login
+                    return render_template('home.html', user = session['username'])
+                except:
+                    error = "Password is incorrect. Try again"
+                    return render_template('login.html', error=error)
+            else:
+                error = "User does not exist"
+                return render_template('login.html', error=error)
 
+    return render_template('login.html')
 
 @app.route('/register')
 def register():
